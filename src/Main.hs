8f370397f52221a8edcd8747d5e09363add3fa8b
leod <subtle.frustration@proton.me>
2019-03-09 22:44:20 +0100

    Refactor

--- src/Main.hs
+++ src/Main.hs
@@ -2,15 +2,38 @@ module Main where
 
 import Data.Maybe (listToMaybe)
 import Data.Int (Int16)
-import Foreign.Storable (sizeOf, pokeByteOff, peekByteOff)
+import Foreign.Storable (sizeOf, pokeElemOff)
 import Foreign.Ptr (castPtr)
-import Foreign.Marshal.Alloc (mallocBytes)
+import Foreign.Marshal.Alloc (mallocBytes, free)
 import Control.Concurrent (threadDelay)
 
 import Sound.OpenAL as AL
 
-type SampleType = Int16
-sampleBytes = sizeOf (0 :: SampleType)
+import Sample (Sampler, sine, mix, sample)
+
+amplitudeToInt :: Double -> Int16
+amplitudeToInt x =
+    let clip = min 1.0 $ max (-1.0) x
+        shift = clip * 0.5 + 0.5
+    in floor $ 32760.0 * clip
+
+samplesToBuffer :: Int -> [Double] -> IO AL.Buffer
+samplesToBuffer sampleRate xs = do
+    let arraySize = length xs * (sizeOf (0 :: Int16))
+    array <- fmap castPtr $ mallocBytes arraySize
+
+    let mapping = zip [0..] (map amplitudeToInt xs)
+    mapM_ (uncurry $ pokeElemOff array) mapping
+
+    let memoryRegion = MemoryRegion array (fromIntegral arraySize)
+    let bufferData = AL.BufferData memoryRegion AL.Mono16 (fromIntegral sampleRate)
+
+    buffer <- AL.genObjectName
+    AL.bufferData buffer $= bufferData
+
+    free array
+
+    return buffer
 
 check :: String -> IO (Maybe a) -> IO a
 check what f = f >>= maybe (error $ what ++ " failed") return
@@ -18,20 +41,12 @@ check what f = f >>= maybe (error $ what ++ " failed") return
 boolToMaybe :: Bool -> Maybe ()
 boolToMaybe x = if x then Just () else Nothing
 
-numSeconds = 5
+manysine :: Sampler
+manysine = mix [sine 440.0, sine 660.0, sine 880.0]
+
 sampleRate = 22050
 
-sine :: Int -> Double -> Int -> Double
-sine sampleRate freq t = sin (2.0 * pi * freq / (fromIntegral sampleRate) * fromIntegral t)
-
-manysine :: Int -> Int -> Double
-manysine sampleRate t = (sine sampleRate 440.0 t + sine sampleRate 660.0 t + sine sampleRate 880.0 t) / 3.0
-
-amplitudeToInt :: Double -> Int16
-amplitudeToInt x =
-    let clip = min 1.0 $ max (-1.0) x
-        shift = clip * 0.5 + 0.5
-    in floor $ 32760.0 * clip
+samples = sample manysine 5.0 sampleRate
 
 main :: IO ()
 main = do
@@ -41,22 +56,12 @@ main = do
    context <- check "createContext" $ AL.createContext device []
    AL.currentContext $= Just context
 
-   buffer <- AL.genObjectName
-
-   let numSamples = numSeconds * sampleRate
-
-   let arraySize = numSamples * sampleBytes
-   array <- fmap castPtr $ mallocBytes arraySize
-
-   mapM_ (\t -> pokeByteOff array (t*sampleBytes) (amplitudeToInt $ manysine sampleRate t)) [0..numSamples]
-   AL.bufferData buffer $= AL.BufferData (MemoryRegion array (fromIntegral arraySize))
-                                         AL.Mono16
-                                         (fromIntegral sampleRate)
+   buffer <- samplesToBuffer sampleRate samples
 
    source <- AL.genObjectName
    AL.buffer source $= Just buffer
    AL.play [source]
 
-   threadDelay (numSeconds*1000*1000)
+   threadDelay (10*1000*1000)
 
    check "closeDevice" $ fmap boolToMaybe $ AL.closeDevice device
